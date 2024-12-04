c634ae956b5305c31b4b1da721d3f998f7c76afe
Arman <165942302+Arman11980@users.noreply.github.com>
2024-12-04 19:23:26 +0300

    Update forms.py

--- blog_site/blogsite/blog/forms.py
+++ blog_site/blogsite/blog/forms.py
@@ -1,17 +1,20 @@
 from django import forms
 from .models import Post, Comment
 
+#форма для регистрации
 class UserRegister(forms.Form):
     username = forms.CharField(max_length=30, label='Ваш логин:')
     password = forms.CharField(min_length=8, label='Ваш пароль')
     repeate_password = forms.CharField(min_length=8, label='Повторите ваш пароль')
     age = forms.CharField(max_length=3, label='Ваш возраст')
 
+#форма для поста
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ['author', 'slug']
 
+#форма для комментариев
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
