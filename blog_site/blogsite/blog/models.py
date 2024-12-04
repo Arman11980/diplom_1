1c8459b264714bbb9e47110bb9064e0570ced112
Arman <165942302+Arman11980@users.noreply.github.com>
2024-12-04 19:31:37 +0300

    Update models.py

--- blog_site/blogsite/blog/models.py
+++ blog_site/blogsite/blog/models.py
@@ -1,35 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User #аунтификация пользователей
 from django.template.defaultfilters import slugify
 
-
+#модель для поста
 class Post(models.Model):
-    title = models.CharField(max_length=150, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    slug = models.SlugField(max_length=100, unique=True)
-    body = models.TextField()
-    image = models.ImageField(upload_to="images/", blank=True, null=True)
+    title = models.CharField(max_length=150, unique=True) #заголовок статьи
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #внешний ключ один ко многим
+    slug = models.SlugField(max_length=100, unique=True) # URL-статьи
+    body = models.TextField() # содержание статьи
+    image = models.ImageField(upload_to="images/", blank=True, null=True) #загрузка изображений
+    created_on = models.DateTimeField(auto_now_add=True) #дата создания статьи
+    updated_on = models.DateTimeField(auto_now=True) #дата внесения изменений статьи
 
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
+    class Meta: #метаданные(сортировка статей) в порядке убывания
         ordering = ['-created_on']
 
     def __str__(self):
-        return self.title
+        return self.title #возвращает отображение понятное для человека
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super(Post, self).save(*args, **kwargs)
 
-
+# модель комментариев к посту кем и когда создан
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     comment = models.TextField(max_length=1000)
-
     created_on = models.DateTimeField(auto_now=True)
 
     class Meta:
