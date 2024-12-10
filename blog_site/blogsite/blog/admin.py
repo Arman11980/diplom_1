134e5bec0bd78f30c70fbe7ce1f21333f229279e
Arman <165942302+Arman11980@users.noreply.github.com>
2024-12-10 09:52:50 +0300

    Update admin.py

--- blog_site/blogsite/blog/admin.py
+++ blog_site/blogsite/blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post, Comment
-
+'''
+Панель администрирования позволяет изменять данные не используя код,
+чтоб попасть в панель администрирования нужно зарегистрировать администратора командой в терминале
+createsuperuser
+'''
 #добавление моделей на сайт администрирования
 admin.site.register(Post)
 admin.site.register(Comment)
